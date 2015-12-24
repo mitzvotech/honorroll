e8547aeb72ad33880aaf9f70e087563df5e34c28
V David Zvenyach <vladlen.zvenyach@gsa.gov>
2015-12-24 13:09:46 -0600

    added errorhandling

--- application.py
+++ application.py
@@ -4,3 +4,13 @@ import os
 if __name__ == "__main__":
     application.debug = os.environ.get('ENV_DEBUG', False)
     application.run(host='0.0.0.0')
+
+    ADMINS = ['vdavez@gmail.com']
+    if not application.debug:
+        import logging
+        from logging.handlers import SMTPHandler
+        mail_handler = SMTPHandler('127.0.0.1',
+                                   'capitalprobono@esq.io',
+                                   ADMINS, 'YourApplication Failed')
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
